3f3f7f99498fb1a6e38522363d2683fdb0e9f517
nlpthuy <nlpthuy137@gmail.com>
2025-05-24 10:59:27 +0700

    Fix Procfile to bind Puma to

--- lib/url_validator.rb
+++ lib/url_validator.rb
@@ -2,7 +2,6 @@ require 'uri'
 require 'ipaddr'
 
 module UrlValidator
-  # Kiểm tra định dạng URL
   def self.valid_format?(url)
     uri = URI.parse(url)
     return %w[http https].include?(uri.scheme) && !uri.host.nil?
@@ -10,19 +9,16 @@ module UrlValidator
     false
   end
 
-  # Kiểm tra URL có phải public và an toàn (không phải IP private, localhost,...)
   def self.safe_public_url?(url)
     uri = URI.parse(url)
     host = uri.host
 
-    # Bỏ qua nếu host là IP private hoặc localhost
     ip = IPAddr.new(host) rescue nil
     if ip
       return false if ip.private? || ip.loopback? || ip.link_local? || ip.multicast?
     else
-      # Nếu không phải IP, kiểm tra nếu hostname là localhost hoặc dạng cục bộ
       return false if host =~ /\Alocalhost\z/i
-      # Thêm domain blacklist nếu cần
+      # Add domain blacklist if need
     end
 
     true
